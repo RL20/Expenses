2125c0bd4d352695b71f43bce227478711cce6d7
RL20 <harel_shalom@yahoo.com>
2022-05-26 03:00:05 +0300

    first design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 34.9,
+      date: new Date(2022, 5, 26),
+    },
+    { id: "e2", title: "New TV", amount: 1799.49, date: new Date(2022, 5, 25) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 2530.67,
+      date: new Date(2022, 5, 24),
+    },
+    {
+      id: "e4",
+      title: "New Desk ",
+      amount: 450,
+      date: new Date(2022, 5, 23),
+    },
+  ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2>Harel</h2>
+      <ExpenseItem date={expenses[0].date} amount={expenses[0].amount} title={expenses[0].title} />
+      <ExpenseItem date={expenses[1].date} amount={expenses[1].amount} title={expenses[1].title} />
+      <ExpenseItem date={expenses[2].date} amount={expenses[2].amount} title={expenses[2].title} />
+      <ExpenseItem date={expenses[3].date} amount={expenses[3].amount} title={expenses[3].title} />
     </div>
   );
 }
